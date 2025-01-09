5a00f2238635195b75b8504559c4a41b39ba8e0e
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2025-01-09 21:20:14 +0330

    docs : add comment for last change

--- Radix-Algorithm/app.js
+++ Radix-Algorithm/app.js
@@ -5,7 +5,11 @@
 // Functions for radix sort algorithm (get array and return a sorted array)
 function radixSort(array) {
 
-
+    /*
+    1 - "Math.max" (method to find the largest number)
+        ## (finglish) in kar moshakhas mikonad ta che martabe i bayad moratab sazi anjam beshe (1,10,100,100,...)
+    2 - "...array" (Convert array to separate numbers)
+    */
     const maxNumber = Math.max(...array)
 
 
